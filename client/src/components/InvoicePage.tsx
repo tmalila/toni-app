094d1837e8e746f3c463d39bb379ffe8b0f4589c
Toni Malila <toni.malila@ecraft.com>
2019-12-25 14:02:39 +0200

    added new invoice button

--- client/src/components/InvoicePage.tsx
+++ client/src/components/InvoicePage.tsx
@@ -6,6 +6,7 @@ import { InvoiceType } from "./Invoice";
 import { List } from "immutable";
 import { useDispatch, useSelector } from "react-redux";
 import { GET_INVOICES } from "../ducks/invoice";
+import { Link, useHistory } from "react-router-dom";
 
 interface Props {
 };
@@ -14,6 +15,7 @@ const InvoicePage: React.FunctionComponent<Props> = props => {
 
   const dispatch = useDispatch();
   const invoices: List<InvoiceType> = useSelector((state: any) => state.invoice.toList());
+  const history = useHistory();
   
   useEffect(() => {
     dispatch({
@@ -21,6 +23,9 @@ const InvoicePage: React.FunctionComponent<Props> = props => {
     });
   }, [dispatch]);
 
+  const navigateToNewInvoice = () => {
+    history.push("/addinvoice")
+  }
 
   return(
     <div
@@ -31,6 +36,9 @@ const InvoicePage: React.FunctionComponent<Props> = props => {
       })}
     >
       <InvoiceList invoices={invoices}></InvoiceList>
+      <button onClick={() => navigateToNewInvoice()}>
+        New Invoice
+      </button>
     </div>
   );
 }
