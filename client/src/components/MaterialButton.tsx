c97872baea78c8ce9f0128cdc45c6d261bd19753
Toni Malila <toni.malila@ecraft.com>
2020-01-12 11:56:26 +0200

    added styles for Button-component

--- client/src/components/MaterialButton.tsx
+++ client/src/components/MaterialButton.tsx
@@ -1,13 +1,18 @@
 import styled from "@emotion/styled";
 
-const MaterialButton = styled.button({
-  border: "5px solid red",
-  borderRadius: "5px",
-  background: "blue",
-  color: "white",
-  "&:disabled": {
-    backgroundColor: "red"
-  }
-});
+const MaterialButton = styled.button(({theme}: any) => ({
+  border: "none",
+  cursor: "pointer",
+  display: "inline-block",
+  fontSize: "1rem",
+  fontWeight: "bold",
+  outline: "none",
+  overflow: "hidden",
+  padding: "0.7rem 0.7rem",
+  textTransform: "uppercase",
+  verticalAlign: "middle",
+  borderRadius: "0.5rem",
+  background: theme.colors.cyan,
+}));
 
 export default MaterialButton;
