313b9185964c360f9e87093f5160d25628fc6697
Toni Malila <toni.malila@ecraft.com>
2020-01-12 12:18:23 +0200

    added ripple effect to button

--- client/src/components/MaterialButton.tsx
+++ client/src/components/MaterialButton.tsx
@@ -13,6 +13,18 @@ const MaterialButton = styled.button(({theme}: any) => ({
   verticalAlign: "middle",
   borderRadius: "0.5rem",
   background: theme.colors.cyan,
+  "&:hover": {
+    // background: theme.colors.cyanlight,
+    background: theme.colors.cyanlight + " radial-gradient(circle, transparent 1%, " + theme.colors.cyanlight + " 1%) center/15000%"
+  },
+  "&:active": {
+    backgroundColor: "#6eb9f7",
+    backgroundSize: "100%",
+    transition: "background 0s",
+  },
+  boxShadow: theme.shadows.depth1,
+  backgroundPosition: "center",
+  transition: "background 0.8s"
 }));
 
 export default MaterialButton;
