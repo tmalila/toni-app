cfb779047668c123171b7f972efcf16cf550bc12
Toni Malila <toni.malila@ecraft.com>
2020-01-20 18:43:07 +0200

    added endpoint and logic for image upload

--- server/src/controllers/imageController.ts
+++ server/src/controllers/imageController.ts
@@ -2,14 +2,13 @@ import * as express from 'express';
 import { InvoiceModel } from '../models/invoice';
 import { MongooseDocument } from 'mongoose';
 import multer from 'multer';
-import { BlobClient, BlockBlobClient } from '@azure/storage-blob';
-import { v4 } from 'uuid';
+import { BlobClient, BlockBlobClient, BlockBlobUploadStreamOptions,  } from '@azure/storage-blob';
+import { v4 as uuid } from 'uuid';
 import * as intoStream from 'into-stream';
  
 const imageRoutes = express.Router();
-const AZURE_STORAGE_CONNECTION_STRING: string = process.env.AZURE_STORAGE_CONNECTION_STRING || "";
 const uploadStrategy = multer({ storage: multer.memoryStorage() }).single('image');
-const CONTAINER_NAME = "images";
+const CONTAINER_NAME = "dev-toniapp-images";
  
 // imageRoutes.get('/invoices', async (req: express.Request, resp: express.Response, next: express.NextFunction) => {
 //     InvoiceModel.find({}, (error: Error, invoices: MongooseDocument) => {
@@ -21,10 +20,27 @@ const CONTAINER_NAME = "images";
 // });
  
 imageRoutes.post('/image/upload', uploadStrategy, async (req: express.Request, resp: express.Response, next: express.NextFunction) => {
-  const blobName = req.file.originalname + v4.toString();
-  const blobClient = new BlockBlobClient(AZURE_STORAGE_CONNECTION_STRING, CONTAINER_NAME, "blobName");
-  const fileStreamObj = intoStream.object(req.file);
-  blobClient.uploadStream(fileStreamObj, req.file.size);
+  const newGuid: string = uuid();
+  const blobName = newGuid + "_" + req.file.originalname;
+  try {
+    const azureConn = process.env.AZURE_STORAGE_CONNECTION_STRING as string;
+    const blobClient = new BlockBlobClient(azureConn, CONTAINER_NAME, blobName);
+    const fileStreamObj = intoStream.default(req.file.buffer);
+    const contType = req.file.mimetype;
+    const options = { blobHTTPHeaders: { blobContentType: contType} } as BlockBlobUploadStreamOptions;
+    const result = await blobClient.uploadStream(fileStreamObj, undefined, undefined, options);
+    if(result.errorCode) {
+      next(result._response.request);
+    }
+    else {
+      resp.json(blobName);
+    }
+  }
+  catch(exception) {
+    console.log("Threw exception: ", exception);
+    next(exception);
+  }
+  
 }); 
  
 export { imageRoutes }
