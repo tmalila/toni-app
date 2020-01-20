674ad39aa16d43118fff720c9b9cf072a1f3bc69
Toni Malila <toni.malila@ecraft.com>
2020-01-20 18:48:50 +0200

    fixing conflicts

--- server/src/controllers/imageController.ts
+++ server/src/controllers/imageController.ts
@@ -2,7 +2,6 @@ import * as express from 'express';
 import { InvoiceModel } from '../models/invoice';
 import { MongooseDocument } from 'mongoose';
 import multer from 'multer';
-<<<<<<< HEAD
 import { BlobClient, BlockBlobClient, BlockBlobUploadStreamOptions,  } from '@azure/storage-blob';
 import { v4 as uuid } from 'uuid';
 import * as intoStream from 'into-stream';
@@ -10,16 +9,6 @@ import * as intoStream from 'into-stream';
 const imageRoutes = express.Router();
 const uploadStrategy = multer({ storage: multer.memoryStorage() }).single('image');
 const CONTAINER_NAME = "dev-toniapp-images";
-=======
-import { BlobClient, BlockBlobClient } from '@azure/storage-blob';
-import { v4 } from 'uuid';
-import * as intoStream from 'into-stream';
- 
-const imageRoutes = express.Router();
-const AZURE_STORAGE_CONNECTION_STRING: string = process.env.AZURE_STORAGE_CONNECTION_STRING || "";
-const uploadStrategy = multer({ storage: multer.memoryStorage() }).single('image');
-const CONTAINER_NAME = "images";
->>>>>>> afca4f615d966b92aa650332843fa4c01a5dd36e
  
 // imageRoutes.get('/invoices', async (req: express.Request, resp: express.Response, next: express.NextFunction) => {
 //     InvoiceModel.find({}, (error: Error, invoices: MongooseDocument) => {
@@ -31,7 +20,6 @@ const CONTAINER_NAME = "images";
 // });
  
 imageRoutes.post('/image/upload', uploadStrategy, async (req: express.Request, resp: express.Response, next: express.NextFunction) => {
-<<<<<<< HEAD
   const newGuid: string = uuid();
   const blobName = newGuid + "_" + req.file.originalname;
   try {
@@ -53,12 +41,6 @@ imageRoutes.post('/image/upload', uploadStrategy, async (req: express.Request, r
     next(exception);
   }
   
-=======
-  const blobName = req.file.originalname + v4.toString();
-  const blobClient = new BlockBlobClient(AZURE_STORAGE_CONNECTION_STRING, CONTAINER_NAME, "blobName");
-  const fileStreamObj = intoStream.object(req.file);
-  blobClient.uploadStream(fileStreamObj, req.file.size);
->>>>>>> afca4f615d966b92aa650332843fa4c01a5dd36e
 }); 
  
 export { imageRoutes }
