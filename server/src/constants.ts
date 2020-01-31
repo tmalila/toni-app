ed9616e67db56e03195c9f9954eb1393c4e4f19a
Toni Malila <toni.malila@ecraft.com>
2020-01-31 15:45:47 +0200

    containerized express backend and mongo

--- server/src/constants.ts
+++ server/src/constants.ts
@@ -1,3 +1,3 @@
 export const PORT = 9001;
 export const WELCOME_MESSAGE = "Welcome to Toni-App Api";
-export const MONGO_URL = "mongodb://localhost:27017/Toni";
+export const MONGO_URL = "mongodb://mongo:27017/Toni";
